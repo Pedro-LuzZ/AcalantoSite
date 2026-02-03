a6f4c154e3beb03ff9d4b93428230f6343272543
Pedro Henrique <pedrohluzcastro@gmail.com>
2026-02-03 18:56:47 -0300

    Rename components to PascalCase

--- site-acalanto/app/page.tsx
+++ site-acalanto/app/page.tsx
@@ -1,9 +1,9 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import QuemSomos from "@/components/quemsomos";
-import Servicos from "@/components/servicos";
-import Infraestrutura from "@/components/infraestrutura";
-import Localizacao from "@/components/localizacao";
+import QuemSomos from "@/components/QuemSomos";
+import Servicos from "@/components/Servicos";
+import Infraestrutura from "@/components/Infraestrutura";
+import Localizacao from "@/components/Localizacao";
 import Footer from "@/components/Footer";
 
 export default function Home() {
